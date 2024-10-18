59e084867e7eea7ff4802904268b3008a3a9a416
DThang200 <thangdao202@gmail.com>
2024-10-18 17:48:59 +0700

    update

---
+++ pages/login.vue
@@ -0,0 +1,48 @@
+<template>
+  <div style="padding: 32px">
+    <label for="input_username">username</label>
+    <input id="input_username" type="text" v-model="username">
+    <label for="input_username">Password</label>
+    <input id="input_username" type="password" v-model="password">
+    <button @click="onLogin">Login</button>
+    <div>
+      response : {{response}}
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data() {
+    return {
+      username: '',
+      password: '',
+      response: {},
+    }
+  },
+  mounted() {
+  },
+  methods: {
+    async onLogin() {
+      const res = await this.$axios.$post('https://frontend.robloxmanager.com/v1/login', {
+        username: this.username,
+        password: this.password,
+      })
+      if (res) {
+        this.response = res
+      } else {
+        this.response = 'Đăng nhập lại'
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.page-content-example {
+  gap: 16px;
+}
+</style>
