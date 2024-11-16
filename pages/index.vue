fa1b5b1d9c7674950f8a21f52e256b95f44496f8
DThang200 <thangdao202@gmail.com>
2024-11-16 12:02:47 +0700

    up

--- pages/index.vue
+++ pages/index.vue
@@ -505,7 +505,7 @@ export default {
 
                                 ["Epic"] = true,
 
-                                ["Legendary"] = false,
+                                ["Legendary"] = true
 
                                 ["Mythical"] = false,
 
