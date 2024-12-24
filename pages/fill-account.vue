cc5290d2e8d73733ba98884e7b4a2fdb35d90ee2
DThang200 <thangdao202@gmail.com>
2024-12-24 23:41:25 +0700

    up

--- pages/fill-account.vue
+++ pages/fill-account.vue
@@ -75,7 +75,6 @@ export default {
           value?.devices.forEach(device => {
             list_device.push(device)
           })
-          console.log('display',list_device)
           this.roblox_data = JSON.parse(JSON.stringify(list_device))
         }
       },deep: true
@@ -119,8 +118,8 @@ export default {
           }
         })
         if (total_account > 0) {
-          if (device?.total_accounts < this.total_account) {
-            const needAcc = this.total_account - device?.total_accounts
+          if (device?.total_accounts < total_accounts) {
+            const needAcc = total_accounts - device?.total_accounts
             listAccFill = listEmptyAcc.slice(getAccIndex, getAccIndex + needAcc)
             getAccIndex = getAccIndex + needAcc
           }
