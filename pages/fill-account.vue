e356c5c93c09c200f7e3b66b94c07c78c087fb76
DThang200 <thangdao202@gmail.com>
2024-12-24 23:49:20 +0700

    up

--- pages/fill-account.vue
+++ pages/fill-account.vue
@@ -124,13 +124,13 @@ export default {
             getAccIndex = getAccIndex + needAcc
           }
           console.log('listAccFill',device?.device_name,listAccFill,listAccFill)
-          if (listAccFill?.length > 0){
-            await this.$axios.$post(`https://frontend.robloxmanager.com/v1/devices/${device?.device_id}/bulk/accounts`, listAccFill,{
-              headers: {
-                'x-auth-token': JSON.parse(localStorage.getItem('token_roblox')) || this.$config.TOKEN_ROBLOX,
-              },
-            });
-          }
+          // if (listAccFill?.length > 0){
+          //   await this.$axios.$post(`https://frontend.robloxmanager.com/v1/devices/${device?.device_id}/bulk/accounts`, listAccFill,{
+          //     headers: {
+          //       'x-auth-token': JSON.parse(localStorage.getItem('token_roblox')) || this.$config.TOKEN_ROBLOX,
+          //     },
+          //   });
+          // }
         }
       }
       this.remain_acc = []
