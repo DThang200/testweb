d116ee6f0ac08c692a632e20120c4a306d1e6a06
DThang200 <thangdao202@gmail.com>
2025-01-09 22:08:45 +0700

    up

--- pages/fill-account.vue
+++ pages/fill-account.vue
@@ -62,7 +62,7 @@ export default {
       farmOption : [
         // {code : 'bloxFruit-maru',label : 'Blox Fruit-Maru',game_id: '2753915549',total_account: 22},
         {code : 'bloxFruit-2600',label : 'Blox Fruit-2550',game_id: '2753915549',total_account: 22},
-        {code : 'bloxFruit-fruit',label : 'Blox Fruit-X3',game_id: '2753915549',total_account: 44},
+        {code : 'bloxFruit-fruit',label : 'Blox Fruit-X3',game_id: '2753915549',total_account: 44,active_account : 22},
         {code : 'bloxFruit-25tab',label : 'Blox Fruit-MagmaV2',game_id: '2753915549',total_account: 25},
         {code : 'ttd-pvp',label : 'TTD-PvP',game_id: '13775256536',total_account: 25},
         // {code : 'Fisch-lv500',label : 'Fisch-lv500',game_id: '16732694052',total_account: 22},
@@ -153,7 +153,7 @@ export default {
         let total_account = 0;
         this.farmOption.forEach(scr => {
           if (scr?.code === this.map_device_data[device?.device_id]?.script){
-            total_account = scr?.total_account
+            total_account = scr?.active_account || scr?.total_account
           }
         })
         const listAccount = await this.$axios.$get(`https://frontend.robloxmanager.com/v1/devices/${device?.device_id}/accounts`,{
