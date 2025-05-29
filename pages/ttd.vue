1e956494462a91f3895ccffae3158eec171ba8b3
DThang200 <thangdao202@gmail.com>
2025-05-29 18:47:09 +0700

    up

--- pages/ttd.vue
+++ pages/ttd.vue
@@ -1053,6 +1053,7 @@ export default {
       const arr = Object.entries(map_device_data)
       for (let i = 0; i < arr.length; i++) {
         const device = arr[i]
+        console.log("device[0]",device[0],!this.activeDevice.includes(device[0]))
         if (device[1]?.script && (device[1]?.script.includes('ttd-') || device[1]?.script.includes('awp-')) && !this.activeDevice.includes(device[0])) {
           const responseCompleted = await this.$axios.$post(`https://frontend.robloxmanager.com/v1/devices/${device[0]}/start`, {},{
             headers: {
