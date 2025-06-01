1076288d305e2c5b6bf62d0eac3a860313bcfccd
DThang200 <thangdao202@gmail.com>
2025-06-01 23:30:32 +0700

    up

--- pages/ttd.vue
+++ pages/ttd.vue
@@ -804,8 +804,8 @@ export default {
                   ["Plant Candy"] = false,
                   ["PetNeedSend"] = {"Red Fox", "Dragonfly", "Raccoon", "Queen Bee"},
                   ["Destroy Mode"] = {
-                      ["Auto Destroy when have money"] = 500000000, -- its will destroy all trees select when money >= select
-                      ["Trees"] = {"Strawberry", "Blueberry", "Corn", "Tomato", "Apple", "Banana"}
+                      ["Auto Destroy when have money"] = 50000000, -- its will destroy all trees select when money >= select
+                      ["Trees"] = {"Strawberry", "Blueberry", "Corn", "Tomato", "Apple", "Banana", "Coconut"}
                   },
                   ["Url"] = "https://discord.com/api/webhooks/1373337547488628856/tpHmmxFCiu8iaWhQTbTB79BZJy8X9QJ3GtoOzE2t-Krw9NxCxC_NvS05XLG46UNvrrvS", -- Webhook
                   ["Boost FPS"] = true,
