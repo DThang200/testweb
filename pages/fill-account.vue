54ad29a97b7a82b6efa265c01a2b8bcae12006b8
DThang200 <thangdao202@gmail.com>
2025-07-16 02:07:48 +0700

    up

--- pages/fill-account.vue
+++ pages/fill-account.vue
@@ -9,6 +9,7 @@
 <!--          <option value="bgsi">BGSI</option>-->
           <option value="astd">ASTD</option>
           <option value="gag">GAG</option>
+          <option value="gag-bone">GAG-Bone</option>
           <option value="bloxFruit">BloxFruit</option>
         </select>
         <textarea v-model="fill_acc" rows="5" style="width: 500px"/>
