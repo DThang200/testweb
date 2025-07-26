60894cf747201167e8b5ca79f84dcecb2080db61
DThang200 <thangdao202@gmail.com>
2025-07-26 22:54:59 +0700

    up

--- pages/fill-account.vue
+++ pages/fill-account.vue
@@ -115,7 +115,7 @@ export default {
         {code : 'awp-bf-2650',label : 'TTD-PvP',game_id: '13775256536',total_account: 35},
         {code : 'astd',label : 'astd',game_id: '17687504411',total_account: 35},
         {code : 'farm-gag',label : 'farm-gag',game_id: '126884695634066',total_account: 30},
-        {code : 'gag-bone',label : 'GAG-Bone',game_id: '126884695634066',total_account: 35},
+        {code : 'gag-bone',label : 'GAG-Bone',game_id: '126884695634066',total_account: 40},
         // {code : 'Fisch-lv500',label : 'Fisch-lv500',game_id: '16732694052',total_account: 22},
         // {code : 'Fisch-lv750',label : 'Fisch-lv750',game_id: '16732694052',total_account: 22},
       ],
